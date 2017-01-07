4f961afae414f44629b7cfdb62edddcade51d8e3
Aman Gohel <Aman Gohel>
2017-01-07 02:03:04 +0000

    Initial commit

---
+++ app/src/main/res/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by aman_ on 07/01/2017.
+ */
+
+public class Main {
+
+    private string myProperty;
+
+    public string getMyProperty() {
+        return myProperty;
+    }
+
+    public string setMyProperty(String myProperty){
+        this.myProperty = myProperty;
+    }
+}
